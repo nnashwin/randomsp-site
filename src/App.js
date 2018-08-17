6408e15e15a7ead30d6b6f4f4ef3dfba3e3df8e2
Tyler Boright <lupin3.ken@gmail.com>
2018-08-16 21:18:59 -0700

    adds a HoverDiv from the Components file into the App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css'
 
 import React, {Component} from 'react'
 import { StyledNav, StyledHeader } from 'landing-page-components';
+import PHOTO from './dice-blur.jpg'
+import styled from 'styled-components';
+import { HoverDiv } from './Components'; 
 
 class App extends Component {
   render() {
     return (
 	  <div>
 		  <StyledNav links={[{href: '#about', text: "About"}, {href: "#email", text: "Sign Up", isButton: true}]} />
-		  <StyledHeader imageUrl={"https://placekitten.com/1200/700"}></StyledHeader>
-		  <div className="App-instructions App-flex">
-			<img className="App-logo" src={require('./react.svg')}/>
-			<p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-		  </div>
+		  <StyledHeader imageUrl={PHOTO} backgroundPositionY="20%">
+			<HoverDiv>Embrace Random</HoverDiv>
+		  </StyledHeader>
 	  </div>
 	);
   }
