fd6358d3ecca83759b4840f78bbfd2542f9691e6
Tyler Boright <lupin3.ken@gmail.com>
2018-08-16 21:18:34 -0700

    adds a file for extra styled components to be used in the app

---
+++ src/Components.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export const HoverDiv = styled.div`
+	width: 100%;
+	color: white;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 50px;
+`;
