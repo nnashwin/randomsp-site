cb434055b92554c2ed54edd92f064af769fc7268
ttymed <tyler.boright@gmail.com>
2018-08-27 23:10:23 -0700

    adds hover behavior to the button

--- src/Components.js
+++ src/Components.js
@@ -19,4 +19,8 @@ export const SignUpButton = styled.button`
 	box-sizing: border-box;
 	cursor: pointer;
 	font-size: 1rem;
+	&:hover {
+		background-color: #0069d9;
+		transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+	}
 `;
