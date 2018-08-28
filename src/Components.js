145e5bfacc35855c46e037c86044a9ad41462590
ttymed <tyler.boright@gmail.com>
2018-08-27 23:08:59 -0700

    adds a SignUpButton to the Components folder

--- src/Components.js
+++ src/Components.js
@@ -9,3 +9,14 @@ export const HoverDiv = styled.div`
 	align-items: center;
 	font-size: 50px;
 `;
+
+export const SignUpButton = styled.button`
+	color: white;
+	border: 1px solid transparent;
+	border-radius: .3rem;
+	background-color: #1a95ff;
+	padding: .6rem;
+	box-sizing: border-box;
+	cursor: pointer;
+	font-size: 1rem;
+`;
